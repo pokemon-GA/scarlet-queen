ac2d00129358f2b76bf348df7fb2344962adf7af
Redaction-F <koebijakobiteorhang@gmail.com>
2025-07-30 15:50:23 +0900

    chore: #40

--- crates/scarlet-queen-entrypoint/src/main.rs
+++ crates/scarlet-queen-entrypoint/src/main.rs
@@ -2,6 +2,6 @@ use scarlet_queen_core::pokemon_type::PokemonTypeFWG;
 use scarlet_queen_entrypoint::pokemon_type::test_and_draw;
 
 fn main() {
-    let test_name = "N=100_R=80_005";
-    test_and_draw::<PokemonTypeFWG, 100, 80>(test_name);
+    let test_name = "N=100_R=75_005";
+    test_and_draw::<PokemonTypeFWG, 100, 75>(test_name).unwrap();
 }
--- crates/scarlet-queen-entrypoint/src/pokemon_type.rs
+++ crates/scarlet-queen-entrypoint/src/pokemon_type.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, fmt::Debug, fs::File, io::BufWriter};
+use std::{collections::HashMap, fmt::Debug, fs::{self, File}, io::BufWriter};
 
 use plotters::{chart::{ChartBuilder, ChartContext}, prelude::{BitMapBackend, Cartesian2d, Circle, DrawingArea, IntoDrawingArea}, series::LineSeries, style::{IntoFont, ShapeStyle, WHITE}};
 use scarlet_queen_core::pokemon_type::PokemonType;
 use scarlet_queen_generation::group::PokemonTypeGroup;
 use scarlet_queen_initializer::group::InitializerSample;
 
-use crate::function::{main_loop, MAIN_LOOP};
+use crate::{error::Error, function::{main_loop, MAIN_LOOP}};
 
 pub fn count<P>(loop_result: Vec<Vec<P>>) -> Vec<HashMap<P, usize>> 
     where 
@@ -58,13 +58,14 @@ pub fn draw_graph<P>(loop_result_count: Vec<HashMap<P, usize>>, img_name: &str)
     }
 }
 
-pub fn test_and_draw<P, const N: usize, const R: usize>(test_name: &str) 
+pub fn test_and_draw<P, const N: usize, const R: usize>(test_name: &str) -> Result<(), Error>
     where 
         P: PokemonType + Debug
 {
+    let dir_path: String = format!("./out/{}", test_name);
+    fs::create_dir_all(&dir_path)?;
     let file: BufWriter<File> = BufWriter::new(
-        File::open(format!("./res_{}.txt", test_name))
-            .unwrap_or_else(|_| File::create(format!("./res_{}.txt", test_name)).unwrap())
+        File::create(format!("{}/res_{}.txt", &dir_path, test_name))?
     );
     let result: Vec<Vec<P>> = main_loop::<
         P, 
@@ -75,5 +76,6 @@ pub fn test_and_draw<P, const N: usize, const R: usize>(test_name: &str)
         R
     >(file).unwrap();
     let count: Vec<std::collections::HashMap<P, usize>> = count(result);
-    draw_graph(count, &format!("./img_{}.png", test_name));
+    draw_graph(count, &format!("{}/img_{}.png", &dir_path, test_name));
+    Ok(())
 }
